bcc5e8d3b0d4402eb6e56a5cde5ad4e57f0249b1
JeremyPel <pellazar@sheridancollege.ca>
2024-09-20 08:32:31 -0400

    First comm week 3

--- src/weekthreeproject/WeekThreeProject.java
+++ src/weekthreeproject/WeekThreeProject.java
@@ -15,7 +15,7 @@ public class WeekThreeProject {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        System.out.println("Hello there");
     }
 
 }
