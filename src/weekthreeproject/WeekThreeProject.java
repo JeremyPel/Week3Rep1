c6c1e28148fe0295bb10f2baf5a93d568d78fdcf
JeremyPel <pellazar@sheridancollege.ca>
2024-09-20 08:25:00 -0400

    First comm week 3

---
+++ src/weekthreeproject/WeekThreeProject.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package weekthreeproject;
+
+/**
+ * This CLass:
+ * @author Jeremy Pellazar
+ */
+public class WeekThreeProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+
+}
